ef6bd15230aaaeeffb9a41585d3bfd53ba703cc0
Rafael Ferreira <rafael.gaspar@tecnico.ulisboa.pt>
2023-06-14 11:01:18 +0100

    vr

--- js/scene.js
+++ js/scene.js
@@ -527,8 +527,6 @@ function init() {
     document.body.appendChild(VRButton.createButton(renderer));
 
     renderer.xr.enabled = true;
-    renderer.xr.getCamera().position.copy( camera.position);
-
     renderer.xr.getCamera().lookAt( camera.target );
 
     clock = new THREE.Clock()
